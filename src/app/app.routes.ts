b6cff471a89dc49db0f3577029ccf6ca974bce31
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-03-13 17:00:00 +0530

    Before Implementing the toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { BookingComponent } from './pages/booking/booking.component';
 import { PaymentComponent } from './pages/payment/payment.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent },
     { path: 'booking', component: BookingComponent },
-    { path: 'payment', component: PaymentComponent },
-    { path: '', redirectTo: '/booking', pathMatch: 'full' },
-    //{ path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
