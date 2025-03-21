b3ad4b04892e531cc9dcfc4caafe1865eaa5747d
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-03-21 09:27:38 +0530

    Before Working on Booking Page

--- src/app/pages/toolbar/toolbar.component.ts
+++ src/app/pages/toolbar/toolbar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
@@ -12,14 +13,33 @@ import { AuthService } from '../../services/auth/auth.service';
   standalone: true,
   imports: [CommonModule, RouterModule, MatToolbarModule, 
     MatButtonModule,
+    MatSidenavModule,
     MatIconModule,
      MatMenuModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent {
+  isMobile: boolean = false;
+  showProfileDropdown: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  checkScreenSize() {
+    console.log(window.innerWidth);
+    this.isMobile = window.innerWidth <= 768;
+  }
+
+  toggleMenu() {
+    this.isMobile = !this.isMobile;
+  }
+
+  toggleProfile() {
+    this.showProfileDropdown = !this.showProfileDropdown;
+  }
 
   logout(){
     this.authService.logout(); // Implement logout logic
