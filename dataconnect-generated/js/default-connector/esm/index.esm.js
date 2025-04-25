dbb23e51d62738c9e01744ab7e467bb70cd8dbaa
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2025-04-25 10:05:17 +0530

    Before Adding Cors File

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'riti-booking-app',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'riti-booking-app',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
